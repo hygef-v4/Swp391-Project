8e571a27cc80fd1692feee6554e1dc8618f7d6e8
Khuat Quang Hung <hungsct1702@gmail.com>
2025-05-23 16:36:48 +0700

    rename authentication folder

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/authentication/SignUpController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/authentication/SignUpController.java
@@ -1,4 +1,4 @@
-package org.swp391.hotelbookingsystem.controller;
+package org.swp391.hotelbookingsystem.controller.authentication;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
