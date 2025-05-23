e4c5b2c1037d20e5b918bc1c0dc0dd466c5a54f0
Đào Chí Cường <cuong16032005@gmail.com>
2025-05-23 16:18:59 +0700

    feature: sign-in

---
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/SignInController.java
@@ -0,0 +1,12 @@
+package org.swp391.hotelbookingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SignInController {
+    @GetMapping({"/", "/sign-in"})
+    public String home() {
+        return "page/sign-in";
+    }
+}
