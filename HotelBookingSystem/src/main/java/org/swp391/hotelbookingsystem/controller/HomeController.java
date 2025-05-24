d6819e8613c2564e4d3e45e45b8e301e6d30ca0a
Khuat Quang Hung <hungsct1702@gmail.com>
2025-05-24 17:26:10 +0700

    update homepage title

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/HomeController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/HomeController.java
@@ -9,17 +9,11 @@ import org.swp391.hotelbookingsystem.constant.ConstantVariables;
 
 @Controller
 public class HomeController {
-<<<<<<< Updated upstream
-    // This method handles the root URL ("/") and returns the name of the view to be rendered.
-    // The view name is "index", which typically corresponds to an HTML file in a templates directory.
-    @GetMapping("/")
-    public String home() {
-=======
-    @GetMapping({"/", "/home"})
 
+    @GetMapping({"/", "/home"})
     public String home(Model model) {
-        model.addAttribute(ConstantVariables.PAGE_TITLE, "Hamora | Book Comfortable Rooms in Vietnam");
->>>>>>> Stashed changes
+        model.addAttribute(ConstantVariables.PAGE_TITLE, "Hamora Booking");
+
         return "page/homepage";
     }
 }
