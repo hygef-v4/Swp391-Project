0ed555c4efbf6426c8d0cd1f53701402eb467d08
Khuat Quang Hung <hungsct1702@gmail.com>
2025-05-24 17:07:10 +0700

    update homepage interface

---
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/constant/ConstantVariables.java
@@ -0,0 +1,6 @@
+package org.swp391.hotelbookingsystem.constant;
+
+public class ConstantVariables {
+    public static final String PAGE_TITLE = "pageTitle";
+
+}
--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/HomeController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/HomeController.java
@@ -1,14 +1,25 @@
 package org.swp391.hotelbookingsystem.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.swp391.hotelbookingsystem.constant.ConstantVariables;
 
 @Controller
 public class HomeController {
+<<<<<<< Updated upstream
     // This method handles the root URL ("/") and returns the name of the view to be rendered.
     // The view name is "index", which typically corresponds to an HTML file in a templates directory.
     @GetMapping("/")
     public String home() {
+=======
+    @GetMapping({"/", "/home"})
+
+    public String home(Model model) {
+        model.addAttribute(ConstantVariables.PAGE_TITLE, "Hamora | Book Comfortable Rooms in Vietnam");
+>>>>>>> Stashed changes
         return "page/homepage";
     }
 }
