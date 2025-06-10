3900ffdeabaf8f65877e8f473132cded8fed5a0a
HNam1315 <vunam1315@gmail.com>
2025-06-10 16:02:49 +0700

    update chart

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminDashboardController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminDashboardController.java
@@ -1,5 +1,7 @@
 package org.swp391.hotelbookingsystem.controller.admin;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,12 +11,16 @@ import org.springframework.ui.Model;
 import org.swp391.hotelbookingsystem.constant.ConstantVariables;
 import org.swp391.hotelbookingsystem.model.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.swp391.hotelbookingsystem.repository.BookingRepo;
 import org.swp391.hotelbookingsystem.service.*;
 
 import java.util.*;
 
 @Controller
 public class AdminDashboardController {
+
+    @Autowired
+    BookingService bookingService;
 
     @Autowired
     HotelService hotelService;
@@ -64,6 +70,40 @@ public class AdminDashboardController {
 
         List<Location> getTop5Location = locationService.getTop5Locations();
         model.addAttribute("locationList", getTop5Location);
+
+        // Merge dates from both stats
+        Set<String> allDates = new TreeSet<>();
+        Map<String, Integer> checkInMap = new HashMap<>();
+        Map<String, Integer> checkOutMap = new HashMap<>();
+
+        for (BookingRepo.DailyStat stat : bookingService.getCheckInStats()) {
+            allDates.add(stat.date());
+            checkInMap.put(stat.date(), stat.count());
+        }
+        for (BookingRepo.DailyStat stat : bookingService.getCheckOutStats()) {
+            allDates.add(stat.date());
+            checkOutMap.put(stat.date(), stat.count());
+        }
+
+// Prepare aligned chart data
+        List<String> dateLabels = new ArrayList<>(allDates);
+        List<Integer> checkInData = new ArrayList<>();
+        List<Integer> checkOutData = new ArrayList<>();
+
+        for (String date : dateLabels) {
+            checkInData.add(checkInMap.getOrDefault(date, 0));
+            checkOutData.add(checkOutMap.getOrDefault(date, 0));
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            model.addAttribute("dateLabels", objectMapper.writeValueAsString(dateLabels));
+            model.addAttribute("checkInData", objectMapper.writeValueAsString(checkInData));
+            model.addAttribute("checkOutData", objectMapper.writeValueAsString(checkOutData));
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
         return "admin/admin-dashboard";
     }
