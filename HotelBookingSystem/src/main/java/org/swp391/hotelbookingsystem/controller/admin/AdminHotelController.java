935baa4852a6eee5b69542b717154ffbd1a84c39
HNam1315 <vunam1315@gmail.com>
2025-06-16 22:04:12 +0700

    update adminHotelDetail

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminHotelController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminHotelController.java
@@ -6,12 +6,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.swp391.hotelbookingsystem.constant.ConstantVariables;
 import org.swp391.hotelbookingsystem.model.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.swp391.hotelbookingsystem.service.*;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @Controller
@@ -32,12 +35,16 @@ public class AdminHotelController {
     final
     LocationService locationService;
 
-    public AdminHotelController(HotelService hotelService, RoomService roomService, ReviewService reviewService, UserService userService, LocationService locationService) {
+    final
+    AmenityService amenityService;
+
+    public AdminHotelController(HotelService hotelService, RoomService roomService, ReviewService reviewService, UserService userService, LocationService locationService, AmenityService amenityService) {
         this.hotelService = hotelService;
         this.roomService = roomService;
         this.reviewService = reviewService;
         this.userService = userService;
         this.locationService = locationService;
+        this.amenityService = amenityService;
     }
 
     @GetMapping("/admin-hotel-list")
@@ -75,5 +82,84 @@ public class AdminHotelController {
 
 
         return "admin/admin-hotel-list";
+    }
+
+    @GetMapping("/admin/hotel/view/{hotelId}")
+    public String hotelDetail(
+            @PathVariable("hotelId") int hotelId,
+            @RequestParam(value = "adults", defaultValue = "1") int adults,
+            @RequestParam(value = "children", defaultValue = "0") int children,
+            @RequestParam(value = "rooms", defaultValue = "1") int roomQuantity,
+
+            Model model, HttpSession session
+    ){
+        List<Location> locations = locationService.getAllLocations();
+        model.addAttribute("locations", locations);
+
+        model.addAttribute("adults", adults);
+        model.addAttribute("children", children);
+        model.addAttribute("roomQuantity", roomQuantity);
+
+        Hotel hotel = hotelService.getHotelById(hotelId);
+        hotel.setPolicy(hotel.getPolicy().replace("<li>", "<li class=\"list-group-item d-flex\"><i class=\"bi bi-arrow-right me-2\"></i>"));
+        model.addAttribute("hotel", hotel);
+
+        User user = (User) session.getAttribute("user");
+        boolean favorite = false;
+        if (user != null) {
+            favorite = hotelService.isFavoriteHotel(user.getId(), hotelId);
+        }model.addAttribute("favorite", favorite);
+
+        String redirect = "";
+        if(adults != 1) redirect += "&adults=" + adults;
+        if(children != 0) redirect += "&children=" + children;
+        if(roomQuantity != 1) redirect += "&rooms=" + roomQuantity;
+        model.addAttribute("redirect", redirect);
+
+        String description = hotel.getDescription();
+        if (description != null) {
+            int index = description.indexOf("<br><br><b>");
+            if (index != -1) {
+                model.addAttribute("short", description.substring(0, index));
+                model.addAttribute("long", description.substring(index));
+            } else {
+                if (description.length() > 800) {
+                    model.addAttribute("short", description.substring(0, 800));
+                    model.addAttribute("long", description.substring(800));
+                } else {
+                    model.addAttribute("short", description);
+                    model.addAttribute("long", "");
+                }
+            }
+        }
+
+        model.addAttribute("hotel", hotel);
+
+        List<Room> rooms = roomService.getRoomByHotelId(hotelId);
+        for(Room room : rooms){
+            room.setDescription("<li>Sức chứa: " + room.getMaxGuests() + " Người</li>" + room.getDescription());
+
+            List<Amenity> amenities = amenityService.getRoomAmenities(room.getRoomId());
+            List<AmenityCategory> categories = new ArrayList<>();
+            String category = "";
+            int index = -1;
+
+            for(Amenity amenity : amenities){
+                if(!category.equals(amenity.getCategory().getName())){
+                    category = amenity.getCategory().getName();
+                    categories.add(new AmenityCategory(amenity.getAmenityId(), category, new ArrayList<>()));
+                    index++;
+                }categories.get(index).getAmenities().add(amenity);
+            }
+
+            room.setCategories(categories);
+        }model.addAttribute("rooms", rooms);
+
+        String hotelName = hotel.getHotelName();
+        String encode = URLEncoder.encode(hotelName, StandardCharsets.UTF_8);
+        String map = "https://www.google.com/maps/search/" + encode + "//@" + hotel.getLatitude() + "," + hotel.getLongitude() + ",17z";
+        model.addAttribute("map", map);
+
+        return "admin/admin-hotel-detail";
     }
 }
