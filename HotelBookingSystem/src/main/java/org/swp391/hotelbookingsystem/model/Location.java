36a2eae9e9ad9eed3b0681c4aafd4aefc38a41c8
Khuat Quang Hung <hungsct1702@gmail.com>
2025-06-21 16:25:44 +0700

    add customer page, add chat box

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/model/Location.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/model/Location.java
@@ -1,11 +1,17 @@
 package org.swp391.hotelbookingsystem.model;
-import lombok.*;
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Location {
+public class Location implements Serializable {
+    private static final long serialVersionUID = 1L;
     private int id;
     private String cityName;
     private String imageUrl;
