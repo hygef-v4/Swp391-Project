06fadaa7035650e9fffecea43044b15e0116806a
ThangVoChien <haivo5229@gmail.com>
2025-07-13 22:49:25 +0700

    refactor constant

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminDashboardController.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/controller/admin/AdminDashboardController.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.swp391.hotelbookingsystem.constant.ConstantVariables;
 import org.swp391.hotelbookingsystem.model.Hotel;
 import org.swp391.hotelbookingsystem.model.Location;
 import org.swp391.hotelbookingsystem.model.Review;
