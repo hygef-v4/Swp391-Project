64a0621186a9c083a9df4b15cfebb4276216ec56
Khuat Quang Hung <hungsct1702@gmail.com>
2025-07-22 17:33:51 +0700

    Add blank line in VNPayConfig for readability
    Inserted a blank line before the md5 method in VNPayConfig.java to improve code readability. No functional changes were made.

--- HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/config/VNPayConfig.java
+++ HotelBookingSystem/src/main/java/org/swp391/hotelbookingsystem/config/VNPayConfig.java
@@ -19,6 +19,7 @@ public class VNPayConfig {
     public static String vnp_HashSecret = "7ZXBXLVLJIILITFSX7T91BK11CA7XQ9W";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
+
     public static String md5(String message) {
         String digest = null;
         try {
